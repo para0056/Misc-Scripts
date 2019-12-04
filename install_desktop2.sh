646386ea97e634f98f3b8db117bcf716745d6fbc
Nick Paradis <37662026+para0056@users.noreply.github.com>
2019-12-04 15:24:28 -0500

    modified to select XFCE desktop

--- install_desktop2.sh
+++ install_desktop2.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # ===================================================================
-# Purpose:           To automate the install of xRDP and GNOM.
+# Purpose:           To automate the install of xRDP and XFCE.
 # Parameters:        None
-# Author:            Jason Vriends
+# Author:            Nick Paradis
 # Notes:             Tested against Ubuntu 18.04-LTS on Microsoft Azure as a Linux Custom Script Extension.
 # ===================================================================
 
 # Update package list
 apt-get update
-
-# Install GNOME
-apt-get install -y ubuntu-gnome-desktop
 
 # Install XRDP
 apt-get install -y xrdp
@@ -20,36 +17,17 @@ apt-get install -y xrdp
 sed -e 's/^new_cursors=true/new_cursors=false/g' -i /etc/xrdp/xrdp.ini
 systemctl restart xrdp
 
-# Create ~/.xsessionrc Create ~/.xsessionrc which will export the environment variable for customized settings for Ubuntu.
-D=/usr/share/ubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop
-cat <<EOF > ~/.xsessionrc
-export GNOME_SHELL_SESSION_MODE=ubuntu
-export XDG_CURRENT_DESKTOP=ubuntu:GNOME
-export XDG_DATA_DIRS=${D}
-export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
-EOF
+# Install xubuntu desktop
+sudo apt install -y xubuntu-desktop
 
-# Authentication Required Dialog If using Xorg as session type, Authentication Required dialog will be displayed after creating session.
-cat <<EOF | tee /etc/polkit-1/localauthority/50-local.d/xrdp-color-manager.pkla
-[Netowrkmanager]
-Identity=unix-user:*
-Action=org.freedesktop.color-manager.create-device
-ResultAny=no
-ResultInactive=no
-ResultActive=yes
+# Automate selection of display manager
+apt install -y expect
+$ cat <<EOF | expect
+set timeout -1
+spawn sudo apt install -y xubuntu-desktop
+expect "Default display manager: "
+send "lightdm\n"
+expect eof
 EOF
 
 sudo systemctl restart polkit
-
-# Install GNOME Extensions
-
-## Download Taskbar
-#wget -O /tmp/taskbar.zip "https://extensions.gnome.org/review/download/8043.shell-extension.zip"
-
-## Extract TaskBar
-#sudo mkdir -p /usr/share/gnome-shell/extensions/TaskBar@zpydr/
-#sudo unzip /tmp/taskbar.zip -d /usr/share/gnome-shell/extensions/TaskBar@zpydr/
-
-## Enable TaskBar
-#gsettings get org.gnome.shell enabled-extensions
-#gsettings set org.gnome.shell enabled-extensions "['netspeed@hedayaty.gmail.com', 'weather-extension@xeked.com', 'TaskBar@zpydr']"
